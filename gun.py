89d93a2865f9c29ab22153e58a87c465da545281
Ever-Firs <valeevslavek@gmail.com>
2023-09-16 16:23:11 +0500

    Lesson-1

---
+++ gun.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Gun:
+
+    def __init__(self, screen):
+
+        self.screen = screen
+        self.images = pygame.image.load('images/ship.png')
+        self.rect = self.images.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def output(self):
+        self.screen.blit(self.images, self.rect)
